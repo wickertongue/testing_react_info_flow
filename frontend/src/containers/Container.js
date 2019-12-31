fbf1c3eb5dd088729b471fa63ed2df1907e872e8
hayden <haydenwb@gmail.com>
2019-12-31 09:56:58 +0000

    have linked api with react frontend, can see data in browser react addon. Cors also now enabled. Now to make the data present on screen in component1

--- frontend/src/containers/Container.js
+++ frontend/src/containers/Container.js
@@ -3,14 +3,37 @@ import Component1 from '../components/Component1'
 
 class Container extends Component {
 
-  render () {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      testItems: []
+    }
+  };
+
+async componentDidMount() {
+  const testItemsFetch = await fetch("http://localhost:8080/testItems/")
+  const testItems = await testItemsFetch.json();
+  this.setState({
+    testItems: testItems._embedded.testItems,
+  });
+  this.setState({ isLoading: false })
+};
+
+  render() {
+    const { isLoading } = this.state;
+
+    if (isLoading) {
+      return <p>Loading...</p>;
+    }
+
     return (
       <div>
         <p>This is the Container</p>
-        <Component1/>
+        <Component1 />
       </div>
     )
   }
-}
+};
 
 export default Container;
